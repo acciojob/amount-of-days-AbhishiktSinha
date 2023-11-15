ac964b576bcf2e82fad50ae80d4a1a6c338ad610
AbhishiktSinha <133574794+AbhishiktSinha@users.noreply.github.com>
2023-11-15 14:46:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+	if(isLeapYear(year)) {
+		return 366;
+	}
+	else return 365;
+}
+function isLeapYear(year){
+	if(year % 4 == 0) {
+		if(year % 100 != 0) {
+			return true;
+		}
+		else if(year % 400 == 0) {
+			return true;
+		}
+		else return false;
+	}
+	else return false;
+}
